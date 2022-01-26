0616986f83ab7b91ac9dbeeff64aa41401625388
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2022-01-27 00:45:32 +0700

    homepage

--- netflix/pages/components/navbar_bottom.tsx
+++ netflix/pages/components/navbar_bottom.tsx
@@ -1,10 +1,17 @@
 import styles from '../../styles/components/NavbarBottom.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faHome
+} from '@fortawesome/free-solid-svg-icons'
 
 export default function NavbarBottom() {
     return (
         <div className={styles.navbar_handphone}>
             <ul>
-                <li><a href="">Home</a></li>
+                <li><a href="">
+                    <FontAwesomeIcon icon={faHome} size='2x' />
+                    <p>Home</p>
+                </a></li>
                 <li><a href="">Games</a></li>
                 <li><a href="">Coming Soon</a></li>
                 <li><a href="">Downloads</a></li>
--- netflix/pages/components/navbar_top.tsx
+++ netflix/pages/components/navbar_top.tsx
@@ -2,17 +2,38 @@ import styles from '../../styles/components/NavbarTop.module.scss';
 
 export default function NavbarTop() {
     return (
-        <div className={styles.navbar_laptop}>
-            <div className={styles.logo_laptop}>
-                <h1>Netflix</h1>
+        <>
+            <div className={styles.navbarTop_laptop}>
+                <div className={styles.logo_laptop}>
+                    <h1>Netflix</h1>
+                </div>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">TV Shows</a></li>
+                    <li><a href="">Movies</a></li>
+                    <li><a href="">News And Populer</a></li>
+                    <li><a href="">My List</a></li>
+                </ul>
             </div>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">TV Shows</a></li>
-                <li><a href="">Movies</a></li>
-                <li><a href="">News And Populer</a></li>
-                <li><a href="">My List</a></li>
-            </ul>
-        </div>
+            <div className={styles.navbarTop_handphone}>
+                <div className={styles.nav_header}>
+                    <div className={styles.nav_headerTop}>
+                        <h1>N</h1>
+                        <div className={styles.nav_search}>
+                            <input type="search" placeholder='Search' />
+                        </div>
+                        <h1>O</h1>
+                    </div>
+                    <div className={styles.nav_headerBottom}>
+                        <ul>
+                            <li><a href="">TV Shows</a></li>
+                            <li><a href="">Movies</a></li>
+                            <li><a href="">Categories</a></li>
+                            <li><a href="">V</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
