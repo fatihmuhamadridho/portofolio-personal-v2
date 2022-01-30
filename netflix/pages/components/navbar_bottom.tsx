ec41aa06e2d30a79aeebcf894742ea3b9197f439
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2022-01-30 07:04:00 +0700

    navbar

--- netflix/pages/components/navbar_bottom.tsx
+++ netflix/pages/components/navbar_bottom.tsx
@@ -1,7 +1,10 @@
 import styles from '../../styles/components/NavbarBottom.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-    faHome
+    faHome,
+    faGamepad,
+    faPhotoVideo,
+    faArrowCircleDown,
 } from '@fortawesome/free-solid-svg-icons'
 
 export default function NavbarBottom() {
@@ -9,12 +12,21 @@ export default function NavbarBottom() {
         <div className={styles.navbar_handphone}>
             <ul>
                 <li><a href="">
-                    <FontAwesomeIcon icon={faHome} size='2x' />
+                    <FontAwesomeIcon icon={faHome} className={styles.icon} />
                     <p>Home</p>
                 </a></li>
-                <li><a href="">Games</a></li>
-                <li><a href="">Coming Soon</a></li>
-                <li><a href="">Downloads</a></li>
+                <li><a href="">
+                    <FontAwesomeIcon icon={faGamepad} className={styles.icon} />
+                    <p>Games</p>
+                </a></li>
+                <li><a href="">
+                    <FontAwesomeIcon icon={faPhotoVideo} className={styles.icon} />
+                    <p>Coming Soon</p>
+                </a></li>
+                <li><a href="">
+                    <FontAwesomeIcon icon={faArrowCircleDown} className={styles.icon} />
+                    <p>Downloads</p>
+                </a></li>
             </ul>
         </div>
     )
